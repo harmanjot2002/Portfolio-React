9f4a62808e611068988b9450f2b051657b1f6fdb
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-12-11 19:32:29 +0530

    Profile

--- src/component/HeroImg.js
+++ src/component/HeroImg.js
@@ -12,7 +12,7 @@ const HeroImg = () => {
       <div className="content" style={{zIndex:20}}>
         <h3>Hello, It's Me</h3>
         <h1>Harmanjot Kaur</h1>
-        <h3>And I'm a <span>Frontend Developer</span></h3>
+        <h3>And I'm a <span>Full Stack Developer</span></h3>
         <p>Versatile full-stack developer skilled in MERN (MongoDB, Express, React, Node) stack with expertise in Android app development using React Native. Eager to contribute technical skills and passion for creating engaging web and android applications in a collaborative team environment.Despite being early in my career, my passion for coding and commitment to staying at the forefront of technology make me a valuable asset for projects seeking fresh perspectives and innovative solutions.</p>
         <div className="buttons">
             <Link to="/project" className="btn">Projects</Link>
